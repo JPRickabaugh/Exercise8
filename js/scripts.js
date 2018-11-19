9dfcd019cb9f1abcd7cb5a2b8fbad495e2df3c86
JP_Rickabaugh <jprickabaugh@gmail.com>
2018-11-19 01:24:23 -0500

    draft

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
   console.log('scripts loaded');
 
   var url = 'http://api.open-notify.org/iss-now.json';
+  var url2= 'https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=10.7505&lon=-176.7655' //10.7505, -176.7655 are coordinates at time of coding
   var data = [];
   var html = '';
 
@@ -17,8 +18,26 @@ $(document).ready(function(){
       console.log(data.iss_position.longitude); //
       html += '<p id=#resultstext>' + data.iss_position.latitude + ', ' + data.iss_position.longitude + '</p>';
       console.log(html);
-      $('#results').html(html);
+      $('#coordinates').html(html);
     }
+  });
+  $.ajax({
+    type:'GET',
+    url:url2,
+    dataType:'json',
+    async:true,
+    data:data,
+    success:function(data){
+      var geocodeResult = '';
+      var msg = geocodeResult + '.';
+      var i = '';
+      console.log(msg);
+      if (msg = '.') {
+        msg = 'The ISS was over the ocean at the time of coding this.';
+        console.log(msg);
+        $('#results').html(msg);
+      }
+    },
   });
 
 });
